6d96b89158734487c0bb0397f33d3c2b8d94d0bb
BHARGAVI <bhargavi07.b@gmail.com>
2025-10-24 17:02:21 +0530

    updated

--- FRONTEND/src/VoterApp.jsx
+++ FRONTEND/src/VoterApp.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_URL}`;
+const baseUrl = import.meta.env.VITE_API_URL;
 
 import {
   Button,
